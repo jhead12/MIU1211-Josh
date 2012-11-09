382bd19b06d399a3a14ff512b022f685e42fd75f
Josh <jhead12@fullsail.edu>
2012-11-08 23:48:29 -0800

    assignment 4

--- Project-3/Gold /js/main-new.js
+++ Project-3/Gold /js/main-new.js
@@ -5,7 +5,7 @@ $('#home').on('pageinit', function(){
 });	
 		
 $('#signUp').on('pageinit', function(){
-					$('#displayLink').click(getData);		$('#clear').click(clearLocal);		//$('#submit').click(validate);				var reset = $("#reset").validate();					reset.resetForm();				var myForm = $('#contactForm');			
+					$('#displayLink').click(getData);		$('#clear').click(clearLocal);		$(makeList);				//$('#submit').click(validate);				var reset = $("#reset").validate();					reset.resetForm();				var myForm = $('#contactForm');			
 		    myForm.validate({
 			invalidHandler: function(form, validator) {
 			},
@@ -41,10 +41,10 @@ else{		localStorage.clear();		alert("All record were deleted");		window.locat
 };
 var editItem = function(){
 	var value = localStorage.getItem(this.key);		var item = JSON.parse(value);				toggleControls("off");		//console.log("This is the Console Log " + value);		//Populate the form field with current local storage values		$("#fname").value = item.fname[1];		$('#lname').value = item.lname[1];		$('#pword-chk').value = item.pword[1];		$('#email').value = item.email[1];		$('#adType').value = item.adType[1];			$('#uname').value = item.uname[1];				var radios = document.forms[0].sex;		for(var i = 0; i < radios.length; i++){					if (radios[i].value == "Male" && item.sex[1] == "Male"){				radios[i].setAttribute("checked", "checked");							}else if(radios[i].value == "Female" && item.sex[1] == "Female"){				radios[i].setAttribute("checked", "checked");							}		}		$('#freq').value = item.freq[1];		$('#date').value = item.date[1];		$('#address').value = item.address[1];								//Remove the initial listener from the input "save wisher" button		save.removeEventListener("click", storeData);		//Change Submit button Value to Edit button		$('#submit').value = "Edit User";		var editSubmit = $('#submit');		editSubmit.addEventListener("click", validate);		editSubmit.key= this.key;		//Save the key value established in this function is the property event, that could be used when savin the data.		
-};var	makeList = function (){					var formTag= document.getElementsByTagName('form'),			selectLi = $("#select");			makeSelect= document.createElement('select'),			makeSelect.setAttribute("id", "group");		for (var i = 0, j=adTypeGroup.length; i < j; i++) {
-			var makeOption = document.createElement('option');			var optText = adTypeGroup[i];			makeOption.setAttribute('value', optText);			makeOption.innerHTML = optText;			makeSelect.appendChild(makeOption);			}
-};
+};function makeList(){		var formTag= document.getElementsByTagName('form'),			selectLi = $("#select");			makeSelect= document.createElement('select'),			makeSelect.setAttribute("id", "group");		for (var i = 0, j=adTypeGroup.length; i < j; i++) {
+			var makeOption = document.createElement('option');			var optText = adTypeGroup[i];			makeOption.setAttribute('value', optText);			makeOption.innerHTML = optText;			makeSelect.appendChild(makeOption);
+}			selectLi.append(makeSelect);	};
 
 var toggleControls = function (n){		switch(n){			case "on":				$('#contactForm').css("display","none");				$('#clear').css("display","inline");				$("#displayLink").css("display","none");				$('#addNew').css("display","inline");				$('#ms1').css("display","none");				$('#ms2').css("display","none");				break;			case "off":				$('#contactForm').css("display", "block");				$('#clear').css("display", "inline");				$("#displayLink").css("display", "inline");				$('#addNew').css('display',"none");				$('#items').css("display","none");				$('#ms1').css('display', "inline");				$('#ms2').css('display',"inline");				break;			default:				return false;		}			};var autofillData = function (){		//Then it is put the data in Local Data		for(var n in json){			var id = Math.floor(Math.random()*10001);			localStorage.setItem(id, JSON.stringify(json[n]));			//console.log("this is the local Storage Test");		}
 	 
-}; 			
+}; 	//Variable defailts	var adTypeGroup = ["--Choose A Group--" , "Computers & Electronics", "Educational", "Music","Lifestyle","Parenting", "Animals & Pets","Auto & Cycles","Business & Finance", "Cooking, Food & Beverage", "Entertainment & TV", "Fashion & Style", "Home & Gardening"],		sexValue;		
